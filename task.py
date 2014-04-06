7621962cb8e46c69a458a1378fec4bc54d2ca93e
FGtatsuro <fujiistorage@gmail.com>
2014-04-06 19:42:29 +0900

    Add heroku task for child task of init task.

--- task.py
+++ task.py
@@ -20,6 +20,22 @@ class CustomCommand(Command):
 class Init(CustomCommand):
     description = "Create skelton project for Flask"
     user_options = []
+    sub_commands = [('heroku', None)]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        [self.run_command(sub) for sub in self.get_sub_commands()]
+        print('init')
+
+
+class Heroku(CustomCommand):
+    description = "Create setting files related to Heroku."
+    user_options = []
     sub_commands = []
 
     def initialize_options(self):
@@ -29,6 +45,9 @@ class Init(CustomCommand):
         pass
 
     def run(self):
-        pass
+        with open('Procfile', 'w') as f:
+            f.write('web: python run.py')
+        with open('runtime.txt', 'w') as f:
+            f.write('python-3.3.2')
 
-setup(cmdclass={'init': Init})
+setup(cmdclass={'init': Init, 'heroku': Heroku})
