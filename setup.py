a119f39a4b4de4fa6baa44599a3b80205fadbc8a
FGtatsuro <fujiistorage@gmail.com>
2015-05-20 23:57:08 +0900

    Add Tox command as setup test command.

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
-from setuptools import setup, find_packages
 import os
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        status = tox.cmdline(self.test_args)
+        sys.exit(status)
 
 version = '0.1'
 
@@ -22,16 +36,20 @@ setup(name='flask-boilerplate',
       author_email='fujiistorage@gmail.com',
       url='https://github.com/FGtatsuro/flask-boilerplate',
       license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(exclude=['tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
           'PasteScript'
       ],
+      tests_require=[
+          'tox'
+      ],
       dependency_links=[
           'hg+ssh://hg@bitbucket.org/FGtatsuro/pastescript@python3_support#egg=PasteScript'
       ],
+      cmdclass={'test': Tox},
       entry_points="""
       # -*- Entry points: -*-
       """,
