f111e6d58de99ce08860a6a2b2803c9013dea610
FGtatsuro <fujiistorage@gmail.com>
2015-06-18 03:51:11 +0900

    Application factory loads config object from config key in PasteDeploy setting.

--- flask_boilerplate/templates/+app_name+/__init__.py
+++ flask_boilerplate/templates/+app_name+/__init__.py
@@ -6,10 +6,12 @@ import os
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 
-def create_app(config):
+from .config import settings
+
+def create_app(global_config, **local_conf):
     app = Flask(__name__)
 
-    app.config.from_object(config)
+    app.config.from_object(settings[local_conf['config_key']])
     if app.debug:
         DebugToolbarExtension(app)
 
